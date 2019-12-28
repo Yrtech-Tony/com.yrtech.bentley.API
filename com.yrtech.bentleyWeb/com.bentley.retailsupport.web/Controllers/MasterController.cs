3fc7300fb1df7849845b7b27c5e4191365062750
huohaitao1 <huohaitao1@126.com>
2019-12-28 11:04:07 +0800

    HHT-添加前端网站

---
+++ com.yrtech.bentleyWeb/com.bentley.retailsupport.web/Controllers/MasterController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace com.bentley.retailsupport.web.Controllers
+{
+    public class MasterController : Controller
+    {
+        //
+        // GET: /Master/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SetLanguage()
+        {
+            return Json("");
+        }
+	}
+}

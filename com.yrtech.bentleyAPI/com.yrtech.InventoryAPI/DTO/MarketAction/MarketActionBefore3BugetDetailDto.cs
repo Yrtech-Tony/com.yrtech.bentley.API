d2f07b9486fc3316ece5c2f1c4dde76bf49cb31f
Tony <moujunsheng15@163.com>
2020-01-04 20:32:02 +0800

    d

---
+++ com.yrtech.bentleyAPI/com.yrtech.InventoryAPI/DTO/MarketAction/MarketActionBefore3BugetDetailDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using com.yrtech.bentley.DAL;
+
+namespace com.yrtech.InventoryAPI.DTO
+{
+    [Serializable]
+    public class MarketActionBefore3BugetDetailDto
+    {
+        public int MarketActionId { get; set; }
+        public int SeqNO { get; set; }
+        public string ItemName { get; set; }
+        public string Descs { get; set; }
+        public Nullable<decimal> UnitPrice { get; set; }
+        public Nullable<decimal> Counts { get; set; }
+        public Nullable<decimal> Total { get; set; }
+        public Nullable<int> InUserId { get; set; }
+        public Nullable<System.DateTime> InDateTime { get; set; }
+        public Nullable<int> ModifyUserId { get; set; }
+        public Nullable<System.DateTime> ModifyDateTime { get; set; }
+    }
+}

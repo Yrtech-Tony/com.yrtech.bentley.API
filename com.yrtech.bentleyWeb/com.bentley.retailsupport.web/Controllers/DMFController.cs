57fb49c1aafcd04d2451a0314763f8be63b2ab51
huohaitao1 <huohaitao1@126.com>
2020-01-09 23:14:47 +0800

    HHT-添加费用报销-未完

---
+++ com.yrtech.bentleyWeb/com.bentley.retailsupport.web/Controllers/DMFController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace com.bentley.retailsupport.web.Controllers
+{
+    public class DMFController : BaseController
+    {
+        //
+        // GET: /DMF/
+        public ActionResult DMFItem()
+        {
+            return View();
+        }
+
+        public ActionResult ExpenseAccount()
+        {
+            return View();
+        }
+	}
+}

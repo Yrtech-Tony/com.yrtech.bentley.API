bacf68134b3f9229a06c97298bf1dbf50e10f439
Tony <moujunsheng15@163.com>
2020-01-19 14:48:18 +0800

    添加了 市场基金的部分

---
+++ com.yrtech.bentleyAPI/com.yrtech.InventoryAPI/DTO/DMF/DMFDto.cs
@@ -0,0 +1,21 @@
+﻿using com.yrtech.bentley.DAL;
+using System;
+using System.Collections.Generic;
+
+namespace com.yrtech.InventoryAPI.DTO
+{
+    [Serializable]
+    public class DMFDto
+    {
+        public string ShopId { get; set; }
+        public string ShopCode { get; set; }
+        public string ShopName { get; set; }
+        public string ShopNameEn { get; set; }
+        public string Quarters { get; set; }
+        public int ActualMonthSaleCount { get; set; }
+        public decimal ActualMonthSaleAmt { get; set; }
+        public decimal ActualAmt { get; set; }
+        public decimal DiffAmt { get; set; }
+
+    }
+}
---
+++ com.yrtech.bentleyAPI/com.yrtech.InventoryAPI/DTO/DMF/DMFQuarterMainDto.cs
@@ -0,0 +1,13 @@
+﻿using com.yrtech.bentley.DAL;
+using System;
+using System.Collections.Generic;
+
+namespace com.yrtech.InventoryAPI.DTO
+{
+    [Serializable]
+    public class DMFQuarterMainDto
+    {
+       public List<DMFDto> DMFQuarterList { get; set; }
+       public List<DMFDetailDto> DMFDetailList { get; set; }
+    }
+}

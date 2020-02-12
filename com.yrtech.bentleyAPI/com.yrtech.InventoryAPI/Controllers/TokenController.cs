f9f70a7f5a34c9df62e7611b461beb53cf414d1f
Tony <moujunsheng15@163.com>
2020-02-12 11:55:36 +0800

    添加了TokenControl

---
+++ com.yrtech.bentleyAPI/com.yrtech.InventoryAPI/Controllers/TokenController.cs
@@ -0,0 +1,53 @@
+﻿using System.Web.Http;
+using com.yrtech.InventoryAPI.Service;
+using com.yrtech.InventoryAPI.Common;
+using System.Collections.Generic;
+using System;
+using com.yrtech.bentley;
+using com.yrtech.InventoryAPI.DTO;
+using com.yrtech.bentley.DAL;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace com.yrtech.InventoryAPI.Controllers
+{
+    [RoutePrefix("token/api")]
+    public class TokenController : BaseController
+    {
+        [HttpGet]
+        [Route("Token/TokenCreate")]
+        public APIResult TokenCreate(string encryptString)
+        {
+            try
+            {
+               
+                return new APIResult() { Status = true, Body = CommonHelper.Encode(TokenHelper.EncryptDES(encryptString)) };
+            }
+            catch (Exception ex)
+            {
+                return new APIResult() { Status = false, Body = ex.Message.ToString() };
+            }
+
+        }
+        [HttpPost]
+        [Route("Token/TokenCheck")]
+        public APIResult TokenCheck(TokenDto token)
+        {
+            try
+            {
+                return new APIResult() { Status = true, Body = CommonHelper.Encode(TokenHelper.DecryptDES(token.TokenString)) };
+            }
+            catch (Exception ex)
+            {
+                return new APIResult() { Status = false, Body = ex.Message.ToString() };
+            }
+
+        }
+    }
+    public class TokenDto
+    {
+        public string EncryptString { get; set; }
+        public string TokenString { get; set; }
+    }
+}

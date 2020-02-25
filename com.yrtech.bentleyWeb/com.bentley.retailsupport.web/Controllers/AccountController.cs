45f1b809f02e23f9cb541dceb84757da5d5d6668
Tony <moujunsheng15@163.com>
2020-02-25 09:58:22 +0800

    删除了日志打印

--- com.yrtech.bentleyWeb/com.bentley.retailsupport.web/Controllers/AccountController.cs
+++ com.yrtech.bentleyWeb/com.bentley.retailsupport.web/Controllers/AccountController.cs
@@ -69,8 +69,8 @@ namespace com.bentley.retailsupport.web.Controllers
             client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
             //发送请求并接受返回的值
             String email = TokenHelper.DecryptDES(token);
-            CommonHelper.log(email);
-            Thread.Sleep(1000);
+            //CommonHelper.log(email);
+            //Thread.Sleep(1000);
             if (string.IsNullOrEmpty(email))
             {
                 throw new Exception("该用户数据在系统不存在或邮箱信息不正确，请联系管理员！");
@@ -78,16 +78,16 @@ namespace com.bentley.retailsupport.web.Controllers
             string getUserApi = string.Format("bentley/api/Master/UserInfoSearch?userId=&accountId=&accountName=&shopCode=&shopName&email={0}", email);
             HttpResponseMessage message = client.GetAsync(getUserApi).Result;
             string json = message.Content.ReadAsStringAsync().Result;
-            CommonHelper.log("1");
-            Thread.Sleep(1000);
+            //CommonHelper.log("1");
+            //Thread.Sleep(1000);
             APIResult result = CommonHelper.DecodeString<APIResult>(json);
-            CommonHelper.log("2");
+           // CommonHelper.log("2");
             Thread.Sleep(1000);
             if (result != null && result.Status)
             {
                 List<UserInfoDto> userList = CommonHelper.DecodeString<List<UserInfoDto>>(result.Body);
-                CommonHelper.log("数量:"+userList.Count.ToString());
-                Thread.Sleep(1000);
+                //CommonHelper.log("数量:"+userList.Count.ToString());
+                //Thread.Sleep(1000);
                 if (userList != null && userList.Count == 1)
                 {
                     //CommonHelper.log(userList[0].AccountId+ "  " +userList[0].Password);

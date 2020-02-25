d0aa4ebbd1d65bb3445fc7004af4d1d124350573
huohaitao1 <huohaitao1@126.com>
2020-02-25 17:29:31 +0800

    HHT-界面大小变化是重刷表格

--- com.yrtech.bentleyWeb/com.bentley.retailsupport.web/Scripts/curtom/ManageAgent.js
+++ com.yrtech.bentleyWeb/com.bentley.retailsupport.web/Scripts/curtom/ManageAgent.js
@@ -1,43 +1,47 @@
 
+initTable();
+
 //生成用户数据
-$('#AgentTable').bootstrapTable({
-    pagination: true,
-    striped: true, //是否显示行间隔色
-    pageNumber: 1,
-    pageSize: 10,
-    height: getClientHeight() - 150,
-    pageList: [5, 10, 20, 50],
-    columns: [{
-        title: "{{'Name'|translate}}",
-        field: 'ShopName'
-    }, {
-        title: "{{'EnName'|translate}}",
-        field: 'ShopNameEn'
-    }, {
-        title: "{{'City'|translate}}",
-        field: 'City'
-    }, {
-        title: "{{'Budget'|translate}}",
-        field: 'Budget'
-    }, {
-        title: "{{'Balance'|translate}}",
-        field: 'Balance'
-    }, {
-        title: "{{'AreaName'|translate}}",
-        field: "AreaName"
-    }, {
-        title: "{{'Edit'|translate}}",
-        field: "ShopId",
-        formatter: function (value, row) {
-            var e = '<a href="/Home/AgentEdit?Id=' + value + '" class="btn btn-link" style="width:auto !important"><i class="icon-pencil icon-white"></i>' + $('#G_Edit').val() + '</a>';
-            e += '<a href="javascript:delShop(' + value + ')" class="btn btn-link" style="width:auto !important"><i class="icon-remove icon-white"></i>' + $('#G_Delete').val() + '</a>';
-            return e;
+function initTable() {
+    $('#AgentTable').bootstrapTable({
+        pagination: true,
+        striped: true, //是否显示行间隔色
+        pageNumber: 1,
+        pageSize: 10,
+        height: getClientHeight() - 150,
+        pageList: [5, 10, 20, 50],
+        columns: [{
+            title: $("#T_Name").val(),
+            field: 'ShopName'
+        }, {
+            title: $("#T_EnName").val(),
+            field: 'ShopNameEn'
+        }, {
+            title: $("#T_City").val(),
+            field: 'City'
+        }, {
+            title: $("#T_Budget").val(),
+            field: 'Budget'
+        }, {
+            title: $("#T_Balance").val(),
+            field: 'Balance'
+        }, {
+            title: $("#T_AreaName").val(),
+            field: "AreaName"
+        }, {
+            title: $("#G_Edit").val(),
+            field: "ShopId",
+            formatter: function (value, row) {
+                var e = '<a href="/Home/AgentEdit?Id=' + value + '" class="btn btn-link" style="width:auto !important"><i class="icon-pencil icon-white"></i>' + $('#G_Edit').val() + '</a>';
+                e += '<a href="javascript:delShop(' + value + ')" class="btn btn-link" style="width:auto !important"><i class="icon-remove icon-white"></i>' + $('#G_Delete').val() + '</a>';
+                return e;
+            }
+        }],
+        onPageChange: function (number, size) {
+            window.localStorage.pageNumberAgent = number;
         }
-    }],
-    onPageChange: function (number, size) {
-        window.localStorage.pageNumberAgent = number;
-    }
-});
+    });
+}
 
 function delShop(id) {
     if (window.confirm('确实要删除该经销商吗？')) {

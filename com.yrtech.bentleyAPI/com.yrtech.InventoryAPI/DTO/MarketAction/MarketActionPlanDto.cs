4b2a22fadd13ea07fcf44e8ad4edbff45a114261
Tony <moujunsheng15@163.com>
2020-06-14 11:03:13 +0800

    添加了DTO

---
+++ com.yrtech.bentleyAPI/com.yrtech.InventoryAPI/DTO/MarketAction/MarketActionPlanDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace com.yrtech.InventoryAPI.DTO
+{
+    [Serializable]
+    public class MarketActionPlanDto
+    {
+        public int MarketActionId { get; set; }
+        public Nullable<int> ShopId { get; set; }
+        public string ShopCode { get; set; }
+        public string ShopName { get; set; }
+        public string ShopNameEn { get; set; }
+        public string AreaName { get; set; }
+        public string ActionName { get; set; }
+        public string ActionCode { get; set; }
+        public int? ExpectLeadsCount { get; set; }
+        public decimal? ActivityBudget { get; set; }
+        public Nullable<int> EventTypeId { get; set; }
+        public string EventTypeName { get; set; }
+        public string EventModeName { get; set; }
+        public string Quarter { get; set; }
+        public string MarketActionStatusCode { get; set; }
+        public string MarketActionStatusName { get; set; }
+        public string MarketActionStatusNameEn { get; set; }
+        public string MarketActionTargetModelCode { get; set; }
+        public string MarketActionTargetModelName { get; set; }
+        public string MarketActionTargetModelNameEn { get; set; }
+        public Nullable<System.DateTime> StartDate { get; set; }
+        public Nullable<System.DateTime> EndDate { get; set; }
+        public string ExpenseAccount { get; set; }
+    }
+}
